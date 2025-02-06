d5a81c55e53e7b62a6ae15237da49c11aa3696c9
markshawn2020 <shawninjuly@gmail.com>
2025-02-06 09:35:18 +0800

    fix backend url

--- packages/web/utils/axios.ts
+++ packages/web/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { createClient } from './supabase/client'
 
-const API_BASE = 'http://localhost:3001'
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
 
 const api = axios.create({
   baseURL: API_BASE,
